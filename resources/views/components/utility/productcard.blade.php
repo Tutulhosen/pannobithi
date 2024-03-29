918bd998c7a9a045fb62b55e88d8b75ef95539bd
monower <sadafahmed720@gmail.com>
2024-03-29 10:38:01 +0600

    minor change on the card

--- resources/views/components/utility/productcard.blade.php
+++ resources/views/components/utility/productcard.blade.php
@@ -1,15 +1,15 @@
-<div class="relative bg-white hover:drop-shadow-lg w-[22%]">
+<a href="#" class="relative bg-white hover:drop-shadow-lg w-[22%]">
     <div class="relative">
-        <a href="#" class="block">
+        <div class="block">
             <img class="w-full h-50 object-cover rounded-t-lg" src="{{ asset('dummy_image.jpg') }}" alt="product name">
-        </a>
+        </div>
     </div>
     <div class="px-2 py-2">
         <div class="text-14 font-semibold text-slate-700">
-            <a href="#" class="hover:text-slate-900">
+            <div class="hover:text-slate-900">
                 product name
 
-            </a>
+            </div>
         </div>
         <div class="text-14 font-semibold text-slate-700">
             <span>
@@ -17,4 +17,4 @@
             </span>
         </div>
     </div>
-</div>
+</a>
